395331716150829e680f1dee5f9d75b5786b9a60
ashrafultusar <ashrafulislamtusar@gmail.com>
2025-11-24 20:24:09 +0600

    added all

--- app/(root)/dashboard/(home)/layout.tsx
+++ app/(root)/dashboard/(home)/layout.tsx
@@ -17,7 +17,8 @@ const HomeLayout = ({ children }: { children: ReactNode }) => {
       <Navbar />
       <div className="flex">
         <Sidebar />
-        <section className="flex min-h-screen flex-1 flex-col px-6 pb-6 pt-28 max-md:pb-14 sm:px-14">
+        <section className="flex min-h-screen flex-1 flex-col px-6 pb-6 pt-28 max-md:pb-14 sm:px-14 bg-black text-white">
+
           <div className="w-full">{children}</div>
         </section>
       </div>
